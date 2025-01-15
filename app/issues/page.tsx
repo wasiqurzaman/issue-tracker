2a980fee6624d2b64027c35ca08c1b51c9344c4d
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-16 02:27:28 +0530

    Add IssueStatusBadge component and integrate it into IssuesPage

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/prisma/client";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import IssueStatusBadge from "../components/IssueStatusBadge";
 
 export default async function IssuesPage() {
   const issues = await prisma.issue.findMany();
@@ -29,10 +30,12 @@ export default async function IssuesPage() {
             <Table.Row key={issue.id}>
               <Table.Cell>
                 {issue.title}
-                <div className="block md:hidden">{issue.status}</div>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.status}
+                <IssueStatusBadge status={issue.status} />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {issue.createdAt.toDateString()}
