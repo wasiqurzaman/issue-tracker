2f8e60fcb805cb3752b71811d5a5a6566a8620cc
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-19 16:43:38 +0530

    Implement PATCH endpoint for issue updates and refactor validation schema

---
+++ app/api/issues/[id]/route.tsx
@@ -0,0 +1,34 @@
+import { issueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "invalid issue" }, { status: 404 });
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
