34d38a150d38a1e43af8108e448beb974f7fc28b
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-21 02:20:38 +0530

    Add referrer policy to NavBar and configure headers in Next.js

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -61,6 +61,7 @@ export default function NavBar() {
                     size="2"
                     radius="full"
                     className="cursor-pointer"
+                    referrerPolicy="no-referrer"
                   />
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
