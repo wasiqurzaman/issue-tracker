c8d8223027bb78d7e88a235c6b51f6d07d602e4c
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-20 21:10:13 +0530

    Add DeleteIssueButton componentge

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@radix-ui/themes";
+
+export default function DeleteIssueButton({ issueId }: { issueId: number }) {
+  return <Button color="red">Delete Issue</Button>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 // import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
 import "./globals.css";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const inter = Inter({
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme appearance="light" accentColor="violet">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
