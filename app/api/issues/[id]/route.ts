c378fb8d88ea05cc5e675f502ab35316441c51e7
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-23 02:35:09 +0530

    Update PATCH route and validation schema to support optional assignedToUserId

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -17,10 +17,19 @@ export async function PATCH(
 
   const { id } = await params;
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const { title, description, assignedToUserId } = body;
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "invalid user" }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
@@ -32,7 +41,11 @@ export async function PATCH(
     return NextResponse.json({ error: "invalid issue" }, { status: 404 });
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description },
+    data: {
+      title,
+      description,
+      assignedToUserId,
+    },
   });
 
   return NextResponse.json(updatedIssue);
