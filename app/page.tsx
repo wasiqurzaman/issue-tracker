971ac72c40855c8e72614f9ec3579c9c63274cd4
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-27 00:17:10 +0530

    Implement pagination functionality with URL parameter handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ page: string }>;
+}) {
+  const { page } = await searchParams;
+
   return (
     <div>
       <h1>Hello World!</h1>
-      <Pagination itemCount={100} pageSize={10} currentPage={1} />
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(page)} />
     </div>
   );
 }
