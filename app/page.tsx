3f44e06c1ff9c0794f181d66f39009490bc5be5c
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-27 00:03:12 +0530

    Add Pagination component for navigating

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Pagination itemCount={100} pageSize={10} currentPage={1} />
     </div>
   );
 }
